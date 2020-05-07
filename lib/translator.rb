ebb0a5fb3a22c9b7843c7cf78ecc40d57bd5cc94
Grant Yoshitsu <grantkoji@gmail.com>
2020-05-07 00:12:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(path, western_emoticon)
         return languages[:japanese]
       end
   end
-  puts "Sorry, that emoticon was not found"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, japanese_emoticon)
